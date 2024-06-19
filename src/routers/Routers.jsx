ac0511ee40584c025537c5a7eaeeae43ee729b98
branGitFox <vixfgit@gmail.com>
2024-06-19 16:08:17 +0300

    debug

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -9,7 +9,7 @@ export default function Routers() {
       <Routes >
         {
           NavData.map((route, index) => (
-            <Route key={index} path={route.path} element={< route.page />} students = {students} />))
+            <Route key={index} path={route.path} element={< route.page />}  />))
         }
       </Routes>
     </BrowserRouter>
